45a46dcaf375572a040b55644aeff92b6b0f255d
mark.tiahrt@concentrix.com <mark.tiahrt@concentrix.com>
2020-11-21 15:41:37 -0700

    styled faq and services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,37 @@
-import React from 'react';
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
+import { About, Description, Image, Hide } from "../styles";
 
-function AboutSection(props) {
-    return (
-        <div>
-            <div className="description">
-                <div className="title">
-                    <div className="hide">
-                        <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>true.</h2>
-                    </div>
-                </div>
-                <p>Contact us for any coding and styles stuff you that have.  I am a professional developer with amazing skills.</p>
-                <button>Contact Me</button>
-            </div>
-            <div className="image">
-                <img src={home1} alt="a guy in a picture"/>
-            </div>
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
         </div>
-    )
-}
-
-
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with a camera" />
+      </Image>
+    </About>
+  );
+};
+//Styled Components
 
 export default AboutSection
 
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,34 +1,69 @@
-import React from 'react';
+import styled from "styled-components";
+import {About} from "../styles";
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
                 <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
                 <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What services do you offer?</h4>
                 <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 }
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 export default FaqSection
