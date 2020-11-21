9beced00cdfed375f040e89db84d117950429461
mark.tiahrt@concentrix.com <mark.tiahrt@concentrix.com>
2020-11-21 12:06:05 -0700

    add FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What services do you offer?</h4>
+                <div className="answer">
+                    <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FaqSection
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
-export default function AboutUs() {
-    return (
-        <AboutSection/>
-    )
+
+const AboutUs = () => {
+    return(
+        <>
+            <AboutSection/>
+            <ServicesSection/>
+            <FaqSection/>
+        </>
+    );
 }
+
+export default AboutUs;
