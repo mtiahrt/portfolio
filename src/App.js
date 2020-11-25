afd2225af94980e208ae62e67ba965ac0ef712a9
mark.tiahrt@concentrix.com <mark.tiahrt@concentrix.com>
2020-11-25 12:05:51 -0700

    added page and element animation

--- src/App.js
+++ src/App.js
@@ -4,28 +4,33 @@ import Nav from './components/Nav';
 import MyWork from './pages/MyWork';
 import ContactMe from './pages/ContactMe';
 import MovieDetail from "./pages/MovieDetail";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
+//animation
+import {AnimatePresence} from 'framer-motion';
 
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <MyWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail/>
-        </Route>
-        <Route path="/contact">
-          <ContactMe />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <MyWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail/>
+          </Route>
+          <Route path="/contact">
+            <ContactMe />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
