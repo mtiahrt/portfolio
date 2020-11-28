632d5470349012cca1cf2734da329c05aa4da3a1
mark.tiahrt@concentrix.com <mark.tiahrt@concentrix.com>
2020-11-28 11:05:53 -0700

    added shared component, scroll anim

--- src/animation.js
+++ src/animation.js
@@ -70,3 +70,20 @@ export const sliderContainer = {
     hidden: {opacity: 1},
     show: {opacity: 1, transition: {staggerChildren: 0.15, ease: 'easeOut'}}
 }
+
+export const scrollReveal = {
+    hidden: {
+        opacity: 0, 
+        scale: 1.2, 
+        transition: {
+            duration: .50
+        }, 
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            duration: .50, 
+        }
+    }
+}
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,13 +2,16 @@ import styled from "styled-components";
 import {About} from "../styles";
 import Toggle from "./Toggle";
 import {AnimateSharedLayout} from 'framer-motion';
+import {useScroll} from './useScroll';
+import {scrollReveal} from '../animation';
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
     return (
-        <Faq>
+        <Faq variants={scrollReveal} animate={controls} initial='hidden' ref={element}>
             <h2>Any Questions <span>FAQ</span></h2>
             <AnimateSharedLayout>
-              <Toggle title='How Do I Start?'>
+              <Toggle titleg='How Do I Start?'>
                   <div className="answer">
                       <p>Lorem ipsum dolor sit amet.</p>
                       <p>Id ut laboris ipsum Lorem do laborum reprehenderit pariatur in incididunt consequat ex.</p>
